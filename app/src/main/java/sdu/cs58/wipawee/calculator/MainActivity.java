db6c182e429ae20c74552f32bae07f343aa03e1c
jakyu <jakyu1329@gmail.com>
2018-02-21 11:44:48 +0700

    Add Complete

--- app/src/main/java/sdu/cs58/wipawee/calculator/MainActivity.java
+++ app/src/main/java/sdu/cs58/wipawee/calculator/MainActivity.java
@@ -2,12 +2,38 @@ package sdu.cs58.wipawee.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //1. ประกาศตัวแปล
+  EditText number1EditTaxt, number2EditText;
+  Button addButton;
+  TextView resultTextView;
+  int num1, num2, result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //2. ผูกตัวแปลบน JAVA กับelement บน xml
+        number1EditTaxt = findViewById(R.id.edtNumber1);
+        number2EditText = findViewById(R.id.edtNumber2);
+        addButton = findViewById(R.id.btnAdd);
+        resultTextView = findViewById(R.id.txvResult);
+        //3.สั่งให้ปุ่มดักฟังว่าเมื่อ user คลิกที่ปุ่ม lodin
+        addButton .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // แปลงค่าที่รับจาก EdiText เป็นตัวเลข เก็บเป็นตัวแปลชนิด int
+                num1 = Integer.parseInt(number1EditTaxt.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result = num1 + num2;
+        // 4. แสดงผลรับการคำนวณ
+                resultTextView.setText(result + " ");
+            }
+        }); //end onclicklistener
+    }  // end onCreate
+}  // end class
